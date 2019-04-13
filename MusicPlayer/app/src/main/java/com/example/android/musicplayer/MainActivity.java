50a6467c98ea4134e3bddd274f144058040e653a
Pratyush Agnihotri <agnihotri@axxessio.com>
2019-04-13 18:44:52 +0200

    Miwwork app and music app

---
+++ MusicPlayer/app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.android.musicplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.cellos_game_of_thrones);
+
+        Button playButton = (Button) findViewById(R.id.play_button);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Playing sound",Toast.LENGTH_SHORT).show();
+                        mediaPlayer.start();
+                        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                            @Override
+                            public void onCompletion(MediaPlayer mp) {
+                                Toast.makeText(MainActivity.this, "I am done!",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+
+        Button pauseButton = (Button) findViewById(R.id.pause_button);
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Pausing sound",Toast.LENGTH_SHORT).show();
+                        mediaPlayer.pause();
+            }
+        });
+    }
+}
